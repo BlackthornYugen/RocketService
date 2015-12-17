9b7364caea3930d20cea3f23d1625fff07dda596
John Steel <john@steelcomputers.com>
2015-12-16 21:29:29 -0500

    Init

---
+++ ExamPrep1/IRocketService.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace ExamPrep1
+{
+    [ServiceContract]
+    public interface IRocketService
+    {
+        [FaultContract(typeof(NoAvailableLaunchpadFault))]
+
+        [OperationContract(IsInitiating = true, IsTerminating = false)]
+        void NewRocket(Rocket rocket);
+
+        [OperationContract(IsInitiating = true, IsTerminating = false)]
+        Rocket AccessExistingRocket(int launchpadId);
+
+        [OperationContract(IsInitiating = false, IsTerminating = false)]
+        void AddCargo(Cargo cargo);
+
+        [OperationContract(IsInitiating = false, IsTerminating = false)]
+        void RemoveCargo(Cargo cargo);
+
+        [OperationContract(IsInitiating = false, IsTerminating = true)]
+        void LaunchActiveRocket(Location target);
+
+        [FaultContract(typeof(NoAvailableHangerFault))]
+        [OperationContract(IsInitiating = false, IsTerminating = true)]
+        void RemoveActiveRocket();
+    }
+
+    [DataContract]
+    internal class NoAvailableLaunchpadFault
+    {
+        [DataMember]
+        string Message = "There are no available launchpads to place your spacecraft.";
+    }
+
+    [DataContract]
+    internal class NoAvailableHangerFault
+    {
+        string Message = "There are no available hangers to store your spacecraft.";
+    }
+
+    [DataContract]
+    public class Rocket
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public Location Location { get; set; }
+    }
+
+    [DataContract]
+    public class Location
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Terestrial { get; set; }
+    }
+
+    [DataContract]
+    public class Cargo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ ExamPrep1/RocketService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ExamPrep1
+{
+    public class RocketService : IRocketService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public Rocket GetDataUsingDataContract(Rocket composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
